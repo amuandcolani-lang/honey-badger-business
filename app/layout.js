946183da18f02bd942510cd24025210dc8ced484
amuandcolani-lang <amuandcolani@gmail.com>
2026-09-20 11:31:42 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,3 @@
-import './globals.css'
+import "./globals.css";
 export const metadata={title:'Honey Badger Business',description:'Your business, all in one place.'}
 export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
